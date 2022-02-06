1acdd607f63a6ac2b17bdc9589eafbca9337321c
Codydematth Programming <36882040+codydematth@users.noreply.github.com>
2022-02-06 11:05:09 +0100

    Update settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r$kioyaa$hthb5-)@9d5fw17le1*vnl532pr!vchs!4pe6oo2k'
+SECRET_KEY = config('')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = Config('')
 ALLOWED_HOSTS = ["*"]
 
 
